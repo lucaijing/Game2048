3e671e8cdef03b5dfc2c2e4d19410872a41564b8
sophia.lu <525579300@qq.com>
2019-08-11 16:26:58 +0800

    更新了UI界面，更新了格子的相对位置。

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-function newGame() {
 
+$(document).ready(function(){
+    newGame();
+})
+
+function newGame() {
+    init();
+}
+
+
+function init() {
+    for(var i = 0;i < 4;i++){
+        for(var j = 0;j < 4 ;j++){
+            let cssGridItem='#grid-no-'+i+'-'+j;
+            //注意GridItem的css是ID选择器 所以需要加上#
+            //console.log(cssGridItem);
+            $(cssGridItem).css({"top":getTop(i,j),"left":getLeft(i,j)});
+        }
+    }
 }
---
+++ support.js
@@ -0,0 +1,7 @@
+function getTop(i, j) {
+    return 20*(i+1)+i*100;
+}
+
+function getLeft(i, j) {
+    return 20*(j+1)+100*j;
+}

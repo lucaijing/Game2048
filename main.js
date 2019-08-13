42b2bf1972e55a676505e940f102d5dddab997fe
sophia.lu <525579300@qq.com>
2019-08-13 13:09:07 +0800

    click the button, generate random number and show in board.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let board = new Array();
 
 $(document).ready(function(){
     newGame();
@@ -5,16 +6,73 @@ $(document).ready(function(){
 
 function newGame() {
     init();
+    generateOneNum();
+    generateOneNum();
 }
 
-
 function init() {
-    for(var i = 0;i < 4;i++){
-        for(var j = 0;j < 4 ;j++){
+    //格子背景布局
+    for(let i = 0;i < 4;i++){
+        for(let j = 0;j < 4 ;j++){
             let cssGridItem='#grid-no-'+i+'-'+j;
             //注意GridItem的css是ID选择器 所以需要加上#
             //console.log(cssGridItem);
             $(cssGridItem).css({"top":getTop(i,j),"left":getLeft(i,j)});
         }
     }
+    //初始化数值board
+    for(let i = 0; i < 4; i++){
+        board[i] = new Array();
+        for (let j = 0; j < 4; j++){
+            board[i][j] = 0;
+        }
+    }
+    //更新16格数值
+    updateBoardView();
+}
+
+function updateBoardView() {
+    $(".number-item").remove();
+    for (let i = 0; i < 4; i++){
+        for (let j = 0; j < 4; j++){
+            //传入 格子数字 的语句
+            let idName = 'number-no-'+i+'-'+j;
+            let insertDiv = '<div class="number-item" id="'+idName+'"></div>';
+            $("#grid-container").append(insertDiv);
+            let findById = '#' + idName;
+            $(findById).css({"top":getTop(i,j), "left":getLeft(i,j)});
+            if (board[i][j] != 0){
+                let number = board[i][j];
+                $(findById).text(number);
+                $(findById).css({"background": getBackground(number), "color":"#776e65", "font-size":"50px", "font-family":"Arial",
+                });
+            }
+        }
+    }
+}
+
+function generateOneNum() {
+
+    let showNum = Math.random() > 0.5 ? 2 : 4;
+    // console.log(showNum);
+    let iNum = Math.floor(Math.random()*4);//随机生成0-3的位置 行
+    let jNum = Math.floor(Math.random()*4);//随机生成0-3的位置 列
+    while (true){
+        if (board[iNum][jNum] == 0){
+            break;
+        }
+        iNum = Math.floor(Math.random()*4);
+        jNum = Math.floor(Math.random()*4);
+    }
+    board[iNum][jNum] = showNum;
+    // let out = 'board' + iNum + ',' + jNum + ' is ' + board[iNum][jNum];
+    // console.log(out);
+    // console.log('///');
+
+    // for(let i = 0; i < 4; i++){
+    //     for (let j = 0; j < 4; j++){
+    //         console.log(board[i][j]+',');
+    //     }
+    // }
+    updateBoardView();
 }
--- support.js
+++ support.js
@@ -5,3 +5,21 @@ function getTop(i, j) {
 function getLeft(i, j) {
     return 20*(j+1)+100*j;
 }
+
+function getBackground(number) {
+    let color;
+    switch (number) {
+        case 2:return "#ff4466";break;
+        case 4:return "#ede0c8";break;
+        case 8:return "#f2b179";break;
+        case 16:return "#f59563";break;
+        case 32:return "#f67c5f";break;
+        case 64:return "#f65e3b";break;
+        case 128:return "#edcf72";break;
+        case 256:return "#edcc61";break;
+        case 512:return "#9c0";break;
+        case 1024:return "#33b5e5";break;
+        case 2048:return "#09c";break;
+    }
+    return "#cdc0b4";
+}

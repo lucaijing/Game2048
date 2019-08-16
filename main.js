646564cb6260cb3f767cc92f4bcdc12ca4a272c0
sophia.lu <525579300@qq.com>
2019-08-16 17:10:53 +0800

    完成基本功能（计数，上下左右移动累加，提示gameover）

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let board = new Array();
+let score=0;
 
 $(document).ready(function(){
     newGame();
@@ -24,6 +25,7 @@ function inputLeft() {
             }
             if((sum==board[i][j]*2)&&sum!=0){
                 board[i][j]=board[i][j]*2;
+                score+=board[i][j];
                 board[i][k]=0;
             }
         }
@@ -60,31 +62,160 @@ function isGameOver() {
             if (board[i][j] == 0){//有空余格子
                 flag = true;break;
             }
-            if ((board[i][j+1] == board[i][j]) && j<3 ){//一行里有相同的可以消除
-                flag = true; break;
+            if(j<3){
+                if ((board[i][j+1] == board[i][j])){//一行里有相同的可以消除
+                    flag = true; break;
+                }
             }
-            if ((board[i+1][j] == board[i][j]) && i<3 ){//一列里有相同的可以消除
-                flag = true; break;
+            if(i<3){
+                if ((board[i+1][j] == board[i][j])){//一列里有相同的可以消除
+                    flag = true; break;
+                }
             }
         }
     }
-    if(flag == false){alert("Game Over!");}
+    console.log(flag)
+    if(!flag){alert("Game Over!");}
 
 }
 
 function inputRight() {
-    return false;
+    for (let i=0;i<4;i++){//向右累加相同数字,注意即使相同的数字之间空几格也可以
+        for (let j=3;j>0;j--){
+            let k=0;let sum=0;
+            for(k=j-1;k>=0;k--){//找到第一个与board[i][j]相等的数字board[i][k], 并把board[i][j]...board[i][k]累加起来=sum
+                if(board[i][j]==board[i][k]){
+                    sum=0;
+                    for(let z=k;z<=j;z++){
+                        sum+=board[i][z];
+                    }
+                    break;
+                }
+            }
+            if((sum==board[i][j]*2)&&sum!=0){
+                board[i][j]=board[i][j]*2;
+                score+=board[i][j];
+                board[i][k]=0;
+            }
+        }
+    }
+    let newBoard=new Array();
+    for (let i=0;i<4;i++){
+        newBoard[i] = new Array();
+        for (let j=0;j<4;j++){
+            newBoard[i][j] = 0;
+        }
+    }
+
+    for (let i=0;i<4;i++){//向you移动,newboard保存向左移动的数组
+        let count=3;
+        for (let j=3;j>=0;j--){
+            if (board[i][j]!=0){
+                newBoard[i][count--]=board[i][j];
+            }
+        }
+    }
+    for (let i=0;i<4;i++){
+        for (let j=0;j<4;j++){
+            board[i][j]=newBoard[i][j];
+        }
+    }
+
+    updateBoardView();
 }
 
 function inputTop() {
-    return false;
+    for (let i=0;i<4;i++){//向上累加相同数字,注意即使相同的数字之间空几格也可以
+        for (let j=0;j<4;j++){// i 列，j行，k行
+            let k=0;let sum=0;
+            for(k=j+1;k<4;k++){//找到第一个与board[i][j]相等的数字board[i][k], 并把board[i][j]...board[i][k]累加起来=sum
+                if(board[j][i]==board[k][i]){
+                    sum=0;
+                    for(let z=j;z<=k;z++){
+                        sum+=board[z][i];
+                    }
+                    break;
+                }
+            }
+            if((sum==board[j][i]*2)&&sum!=0){
+                board[j][i]=board[j][i]*2;
+                score+=board[j][i];
+                board[k][i]=0;
+            }
+        }
+    }
+    let newBoard=new Array();
+    for (let i=0;i<4;i++){
+        newBoard[i] = new Array();
+        for (let j=0;j<4;j++){
+            newBoard[i][j] = 0;
+        }
+    }
+
+    for (let i=0;i<4;i++){//向上移动,newboard保存向左移动的数组
+        let count=0;//i列 j行
+        for (let j=0;j<4;j++){
+            if (board[j][i]!=0){
+                newBoard[count++][i]=board[j][i];
+            }
+        }
+    }
+    for (let i=0;i<4;i++){
+        for (let j=0;j<4;j++){
+            board[i][j]=newBoard[i][j];
+        }
+    }
+
+    updateBoardView();
 }
 
 function inputBottom() {
-    return false;
+    for (let i=0;i<4;i++){//向上累加相同数字,注意即使相同的数字之间空几格也可以
+        for (let j=3;j>=0;j--){// i 列，j行，k行
+            let k=0;let sum=0;
+            for(k=3;k>j;k--){//找到第一个与board[i][j]相等的数字board[i][k], 并把board[i][j]...board[i][k]累加起来=sum
+                if(board[j][i]==board[k][i]){
+                    sum=0;
+                    for(let z=j;z<=k;z++){
+                        sum+=board[z][i];
+                    }
+                    break;
+                }
+            }
+            if((sum==board[j][i]*2)&&sum!=0){
+                board[j][i]=board[j][i]*2;
+                score+=board[j][i];
+                board[k][i]=0;
+            }
+        }
+    }
+    let newBoard=new Array();
+    for (let i=0;i<4;i++){
+        newBoard[i] = new Array();
+        for (let j=0;j<4;j++){
+            newBoard[i][j] = 0;
+        }
+    }
+
+    for (let i=0;i<4;i++){//向下移动,newboard保存向左移动的数组
+        let count=3;//i列 j行
+        for (let j=3;j>=0;j--){
+            if (board[j][i]!=0){
+                newBoard[count--][i]=board[j][i];
+            }
+        }
+    }
+    for (let i=0;i<4;i++){
+        for (let j=0;j<4;j++){
+            board[i][j]=newBoard[i][j];
+        }
+    }
+
+    updateBoardView();
 }
 
 $(document).keydown(function (event){//按下方向键
+    isGameOver();
     switch (event.which) {
         case 37:
             inputLeft();
@@ -104,7 +235,6 @@ $(document).keydown(function (event){//按下方向键
             generateOneNum();
             //console.log('bottom')
             break;//下
-        default:isGameOver();break;
     }
 
 })
@@ -117,6 +247,7 @@ function newGame() {
 
 function init() {
     //格子背景布局
+    score=0;
     for(let i = 0;i < 4;i++){
         for(let j = 0;j < 4 ;j++){
             let cssGridItem='#grid-no-'+i+'-'+j;
@@ -155,6 +286,7 @@ function updateBoardView() {//刷新游戏界面
         }
     }
     $(".number-item").fadeIn();//动画效果：淡入
+    $("#score").text(score);
     // 将display设置为none，不显示，使用动画效果显示class为number-item的元素
 }
 
@@ -164,14 +296,27 @@ function generateOneNum() {
     // console.log(showNum);
     let iNum = Math.floor(Math.random()*4);//随机生成0-3的位置 行
     let jNum = Math.floor(Math.random()*4);//随机生成0-3的位置 列
-    while (true){
+    let count=0;
+    while (count<30){
         if (board[iNum][jNum] == 0){
             break;
         }
         iNum = Math.floor(Math.random()*4);
         jNum = Math.floor(Math.random()*4);
+        count++;
     }
-    board[iNum][jNum] = showNum;
+    if(count<=30){
+        board[iNum][jNum] = showNum;
+    }
+    else{
+        for(let i=0;i<4;i++){
+            for(let j=0;j<4;j++){
+                if(board[i][j]==0){
+                    board[i][j]=showNum;
+                }
+            }
+        }
+    }
     // let out = 'board' + iNum + ',' + jNum + ' is ' + board[iNum][jNum];
     // console.log(out);
     // console.log('///');
